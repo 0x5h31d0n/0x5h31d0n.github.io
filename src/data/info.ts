e70f8a1a3da66b49524fcc0ab021f164bcc36e46
Sheldon Menezes <samymenezes33@gmail.com>
2023-04-06 21:26:18 +0530

    initial commit

---
+++ src/data/info.ts
@@ -0,0 +1,84 @@
+export const info = {
+  baseUrl: "#",
+  name: "Sheldon Menezes",
+  jobDescription: "Web dev",
+  about: `I am a web developer currently pursuing my engineering at SJEC College. My passion for web development began early on in my academic journey, and since then, I have been honing my skills and expanding my knowledge in this field and also in security part of it. I have experience with  HTML, CSS, JavaScript, and Python, and have worked on various web development projects in the past. At SJEC, I am continuing to build my skills and knowledge through hands-on experience with real-world projects and coursework. I am excited to see where my passion for web development will take me in the future, and am committed to staying up-to-date with the latest technologies and best practices in the field.`,
+
+  experience: [
+    {
+      name: "Wikimedia Foundation",
+      location: "St. Petersburg, Florida, United States",
+      startDate: "Oct 2022",
+      endDate: "Oct 2022",
+      description: [
+        "🐞Helped to identify XSS vulnerability + mitigation to the same and got inducted to Wikimedia Security Hall Of Fame"
+      ],
+    },
+
+    {
+      name: "Sceptix FOSS Web Project",
+      location: "SJEC, Vamanjoor, Mangaluru",
+      startDate: "Jan 2023",
+      endDate: "Feb 2022",
+      description: [
+        "✔️ Created a website dedicated to promoting free and open-source software 🖥️ (FOSS)",
+        "✔️ Demonstrated understanding of FOSS principles and benefits through the design and content of the website",
+        "✔️ Developed a curated list of FOSS tools and applications, including descriptions and links to download",
+        "✔️ Tested the website to ensure that it is accessible and works well on various devices and browsers",
+        "✔️ Presented the completed project to the Sceptix club as a requirement for joining the organization",
+      ],
+    },
+  ],
+
+ education: [
+    {
+      name: "St Joseph Engineering College (SJEC)",
+      location: "Vamanjoor, Mangaluru",
+      startDate: "Aug 2026",
+      endDate: "Aug 2026",
+      description: [
+        "Computer Science Engineering (CSE)",
+      ],
+    },
+    {
+      name: "St. Aloysius PU College",
+      location: "Kodialbail, Mangaluru",
+      startDate: "2020",
+      endDate: "2022",
+      description: ["Physics-Chemistry-Maths-Computer Science[PCMC]"],
+    },
+    {
+      name: "St Aloysius High School",
+      location: "Kodailbail, Mangaluru",
+      startDate: "2017",
+      endDate: "2020",
+      description: ["High Schooling"],
+    },
+  ],
+
+  socialMedia: {
+    github: "https://github.com/0x5h31d0n",
+    email: "mailto:samymenezes33@gmail.com",
+    linkedin: "https://www.linkedin.com/in/sheldon-menezes-8272b124a/",
+  },
+
+  projects: [
+    {
+      title: "Webpage on FOSS",
+      isFeatured: true,
+      thumbnail: "/assets/images/foss.png",
+      githubUrl: "https://github.com/0x5h31d0n/sceptix-recruitment",
+      liveUrl: "https://0x5h31d0n.github.io/sceptix-recruitment/",
+    },
+  ],
+
+  achievement: [
+    {
+      title: "WebFOSS",
+      isFeatured: true,
+      thumbnail: "/assets/images/foss.png",
+      githubUrl: "https://github.com/0x5h31d0n/sceptix-recruitment",
+      liveUrl: "https://0x5h31d0n.github.io/sceptix-recruitment/",
+    },
+  ],
+};
